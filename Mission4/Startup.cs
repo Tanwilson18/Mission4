b834dac535ff11e316a9d1b7e5bcf6506c395323
Tanner Wilson <twilso48@byu.edu>
2023-02-06 23:14:55 -0700

    Mission5

--- Mission4/Startup.cs
+++ Mission4/Startup.cs
@@ -16,6 +16,7 @@ namespace Mission4
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,8 +24,21 @@ namespace Mission4
         // allows program to find files in the root directory 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if(env.IsEnvironment("Devlopment"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+             {
+                 endpoints.MapControllerRoute(
+                     name: "default",
+                     pattern: "{controller=blah}/{action=Index}/{id?}"
+                     );
+             });
         }
     }
 }
